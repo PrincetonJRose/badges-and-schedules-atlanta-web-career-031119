4d0df4a240d9c98fd01e6d6c43685ec9076c7fda
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 00:32:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,8 @@
 # Write your code here.
-def batch_badge_creator
-  
+def batch_badge_creator(names)
+  badges = []
+  names.each { |index| badges.push(badge_maker(names[index])) }
+  return badges
 end
 
 def badge_maker(name)
