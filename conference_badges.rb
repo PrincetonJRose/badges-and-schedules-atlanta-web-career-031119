f6bf9d6b9d6f131aef52634bb1ca850f007b3365
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 00:37:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,10 @@
 # Write your code here.
 def batch_badge_creator(names)
   badges = []
-  names.each { |index| badges.push(badge_maker(names[index])) }
+  # names.each { |index| badges.push(badge_maker(names[index])) }
+  for count in 0...names.length
+    badges.push(badge_maker(names[count]))
+  end
   return badges
 end
 
