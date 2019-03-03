10b56b8ae29fa88c38b76adef4958b0916467d37
Princeton Rose <decoyzpr@gmail.com>
2019-03-03 00:54:24 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
 # Write your code here.
+def batch_badge_creator(names)
+  badges = []
+  # names.each { |index| badges.push(badge_maker(names[index])) }
+  for count in 0...names.length
+    badges.push(badge_maker(names[count]))
+  end
+  return badges
+end
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  for count in 1..speakers.size
+    room_assignments.push("Hello, #{speakers[count-1]}! You'll be assigned to room #{count}!")
+  end
+  return room_assignments
+end
+
+def printer(speakers)
+  badges = batch_badge_creator(speakers)
+  rooms = assign_rooms(speakers)
+  for phrase in badges
+    puts phrase
+  end
+  for room in rooms
+    puts room
+  end
+end
